d76699b3b3700d0ba6b00bc94e88c3ad9fca16a2
h404bi <h404bi@gmail.com>
2017-11-12 16:39:17 +0800

    Add some scripts

---
+++ tools/copy-basic-files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp ~/.dotfiles/src/.bash_profile ~/
+cp ~/.dotfiles/src/.dircolorsdb ~/
+cp ~/.dotfiles/src/.gitconfig ~/
+cp ~/.dotfiles/src/.gitignore_global ~/
+cp ~/.dotfiles/src/.gitconfig ~/
+cp ~/.dotfiles/src/.inputrc ~/
+cp ~/.dotfiles/src/.npmrc ~/
+cp ~/.dotfiles/src/.vimrc ~/
