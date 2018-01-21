83248f393297f75700ba8e2ae23735adf304069b
h404bi <h404bi@gmail.com>
2018-01-22 00:56:17 +0800

    Add mirror to miniconda install script

--- scripts/linux-install-miniconda3.sh
+++ scripts/linux-install-miniconda3.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+ORIGIN=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+MIRROR=https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
 if [[ ! -d $HOME/miniconda3 ]]; then
-  echo "Installing Miniconda3..."
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda3.sh
+  if [ $1 == '-m' ]; then
+    wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda3.sh
+  else
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda3.sh
+  fi
   bash /tmp/miniconda3.sh -b -p $HOME/miniconda3
   export PATH="$HOME/miniconda3/bin:$PATH"
+else
+  echo "Miniconda3 has installed in '$HOME/miniconda3'."
 fi
