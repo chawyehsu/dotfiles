d7228662289c9bc1eab5c3b6f2ad89039088df8f
h404bi <h404bi@gmail.com>
2018-07-24 23:37:48 +0800

    Add cmder reset profile

---
+++ src/scoop/apps/cmder/current/config/user-profile.ps1
@@ -0,0 +1,18 @@
+# Use this file to run your own startup commands
+
+## Prompt Customization
+<#
+.SYNTAX
+    <PrePrompt><CMDER DEFAULT>
+    λ <PostPrompt> <repl input>
+.EXAMPLE
+    <PrePrompt>N:\Documents\src\cmder [master]
+    λ <PostPrompt> |
+#>
+
+[ScriptBlock]$PrePrompt = {}
+[ScriptBlock]$PostPrompt = {}
+[ScriptBlock]$CmderPrompt = {}
+[ScriptBlock]$Prompt = {}
+
+## <Continue to add your own>
