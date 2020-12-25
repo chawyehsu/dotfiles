ac07778d9d80779ebf10cc626dcd9a78f67e3d50
Chawye Hsu <chawyehsu@hotmail.com>
2020-12-26 00:32:01 +0800

    scripts: cleanup
    Signed-off-by: Chawye Hsu <chawyehsu@hotmail.com>

--- scripts/copy-basic-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. ./include/color.sh
-
-# setup basic usage
-if [[ -d $HOME/.dotfiles ]]; then
-  cp $HOME/.dotfiles/src/.{bash_logout,bash_profile,bashrc,dircolorsdb,gitconfig,gitignore_global,inputrc,npmrc,vimrc} $HOME
-  echo "${CSUCCESS}Done.${CEND}"
-fi
