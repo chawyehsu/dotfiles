7893a35d9646658ecde987761e46b9dc1365e125
Ross Smith II <ross@smithii.com>
2021-04-04 06:44:58 -0700

    Fix: curl requires filename for -o, not -O

--- scripts/initialize-scoop.ps1
+++ scripts/initialize-scoop.ps1
@@ -12,8 +12,8 @@ scoop bucket add dorado https://github.com/chawyehsu/dorado
 # Pour out configs
 # colortool schemes
 Push-Location "$env:USERPROFILE\scoop\persist\colortool\schemes"
-curl -o https://raw.githubusercontent.com/chriskempson/base16-iterm2/master/base16-tomorrownight.dark.256.itermcolors
-curl -o https://raw.githubusercontent.com/chriskempson/base16-iterm2/master/base16-tomorrownight.dark.itermcolors
+curl -O https://raw.githubusercontent.com/chriskempson/base16-iterm2/master/base16-tomorrownight.dark.256.itermcolors
+curl -O https://raw.githubusercontent.com/chriskempson/base16-iterm2/master/base16-tomorrownight.dark.itermcolors
 Pop-Location
 
 # Enable PowerShell colorscheme
