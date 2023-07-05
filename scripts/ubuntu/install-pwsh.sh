b495d3bcdfe8203b71076d5a602135f7e6cd2811
Chawye Hsu <su+git@chawyehsu.com>
2023-07-05 23:12:43 +0800

    chore: update scripts
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- scripts/ubuntu/install-pwsh.sh
+++ scripts/ubuntu/install-pwsh.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+# https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu
 # Update the list of packages
 sudo apt-get update
 # Install pre-requisite packages.
 sudo apt-get install -y wget apt-transport-https software-properties-common
 # Download the Microsoft repository GPG keys
-wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
 # Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
-# Update the list of products
+# Delete the the Microsoft repository GPG keys file
+rm packages-microsoft-prod.deb
+# Update the list of packages after we added packages.microsoft.com
 sudo apt-get update
-# Enable the "universe" repositories
-sudo add-apt-repository universe
 # Install PowerShell
 sudo apt-get install -y powershell
