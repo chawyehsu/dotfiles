a328e72751c20980e3c6fabf7ded68389f71ae50
Chawye Hsu <su+git@chawyehsu.com>
2025-08-08 12:04:24 +0800

    scripts: update install-pwsh
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

---
+++ scripts/linux/install-pwsh.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Function to install PowerShell on Ubuntu
+ubuntu_install_pwsh() {
+    # https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu
+    # Update the list of packages
+    sudo apt-get update
+    # Install pre-requisite packages.
+    sudo apt-get install -y wget apt-transport-https software-properties-common
+    # Download the Microsoft repository GPG keys
+    wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+    # Register the Microsoft repository GPG keys
+    sudo dpkg -i packages-microsoft-prod.deb
+    # Delete the the Microsoft repository GPG keys file
+    rm packages-microsoft-prod.deb
+    # Update the list of packages after we added packages.microsoft.com
+    sudo apt-get update
+    # Install PowerShell
+    sudo apt-get install -y powershell
+}
+
+# Function to install PowerShell on Debian
+debian_install_pwsh() {
+    # https://learn.microsoft.com/en-us/powershell/scripting/install/install-debian
+    # Update the list of packages
+    sudo apt-get update
+    # Install pre-requisite packages.
+    sudo apt-get install -y wget
+    # Get the version of Debian
+    # shellcheck disable=SC1091
+    source /etc/os-release
+    # stop if VERSION_ID is not supported
+    if [[ "$VERSION_ID" != "11" && "$VERSION_ID" != "12" ]]; then
+        echo "Unsupported Debian version: $VERSION_ID"
+        exit 1
+    fi
+    # Download the Microsoft repository GPG keys
+    wget -q "https://packages.microsoft.com/config/debian/$VERSION_ID/packages-microsoft-prod.deb"
+    # Register the Microsoft repository GPG keys
+    sudo dpkg -i packages-microsoft-prod.deb
+    # Delete the Microsoft repository GPG keys file
+    rm packages-microsoft-prod.deb
+    # Update the list of packages after we added packages.microsoft.com
+    sudo apt-get update
+    # Install PowerShell
+    sudo apt-get install -y powershell
+}
+
+# stop if not linux os type
+if [[ "$(uname -s)" != "Linux" ]]; then
+    echo "This script is intended for Linux systems only."
+    exit 1
+fi
+
+# Check the distribution and call the appropriate function
+if [[ -f /etc/os-release ]]; then
+    # shellcheck disable=SC1091
+    source /etc/os-release
+    case "$ID" in
+        ubuntu)
+            ubuntu_install_pwsh
+            ;;
+        debian)
+            debian_install_pwsh
+            ;;
+        *)
+            echo "Unsupported Linux distribution: $ID"
+            exit 1
+            ;;
+    esac
+else
+    echo "Cannot determine the Linux distribution."
+    exit 1
+fi
